929d090d804b8cd95e18ecd57e566593b135e5e2
Shah-imran <shahimrans64@gmail.com>
2023-05-18 15:38:20 +0600

    daily commit

--- app/email.py
+++ app/email.py
@@ -13,13 +13,10 @@ def send_async_email(app, msg):
 
 def send_email(to, subject, template, **kwargs):
     app = current_app._get_current_object()
-    print(app.config)
+
     msg = Message(app.config['MAIL_SUBJECT_PREFIX'] + ' ' + subject,
                   sender=app.config['MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    # thr = Thread(target=send_async_email, args=[app, msg])
-    print(msg)
+
     mail.send(msg)
-    # thr.start()
-    # return thr
