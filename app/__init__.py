9bb639f3d1b4f1b720d56a93beb4840d542559bc
Shah-imran <shahimrans64@gmail.com>
2023-05-18 14:51:19 +0600

    daily commit

--- app/__init__.py
+++ app/__init__.py
@@ -16,17 +16,16 @@ login_manager.login_view = 'auth.login'
 
 
 def create_app(config_name):
-    print("here - config name", config_name)
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
-
+    
     bootstrap.init_app(app)
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
-
+    print(app.config)
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint, url_prefix='/')
 
